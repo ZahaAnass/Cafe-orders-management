7175609b47742e7e7507807a50744cf40baee0cd
ZahaAnass <zahaanass277@gmail.com>
2025-07-03 21:03:25 +0100

    remove the init db

--- app.py
+++ app.py
@@ -1,17 +1,15 @@
 import customtkinter as ctk
 from views.dashboard import Dashboard
-from database.database import init_db
 
 class App(ctk.CTk):
     def __init__(self):
         super().__init__()
         self.title("Gestion des Commandes de Café")
-        self.geometry("1100x650")
+        self.geometry("1200x700")
         self.dashboard = Dashboard(self)
         self.dashboard.pack(fill="both", expand=True)
 
 if __name__ == "__main__":
     ctk.set_appearance_mode("light")  # ou "system" ou "dark"
     app = App()
-    # init_db()
     app.mainloop()
